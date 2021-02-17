61b5cec836c6764fcc91e2a9e7ebf2a360a875d2
Jose Bejarano <jabcarranz@gmail.com>
2021-02-17 18:49:54 -0500

    [push_service] Added push service

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Services\FirebaseService;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function pushVerses($book_id){
+
+        $fbase = new FirebaseService();
+
+        return $fbase->writeToFirestore($book_id);
+
+    }
 }
